1aebd40e6cf8ceb1c8725f4304e2d9955be1facf
ilpovirt <99248645+ilpovirt@users.noreply.github.com>
2024-09-19 11:03:27 +0300

    Update streamlit_folium_map.py

--- streamlit_folium_map.py
+++ streamlit_folium_map.py
@@ -3,7 +3,8 @@ import pandas as pd
 import folium 
 from streamlit_folium import st_folium
 
-df = pd.read_csv("https://github.com/ilpovirt/streamlit_map/blob/main/GPSdata.csv")
+url = "https://raw.githubusercontent.com/ilpovirt/streamlit_map/main/GPSdata.csv"
+df = pd.read_csv(url)
 
 #Create a map where the center is at start_lat start_long and zoom level is defined
 start_lat = df['Latitude (°)'].mean()
