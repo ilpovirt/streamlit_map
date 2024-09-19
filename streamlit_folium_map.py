72a2c0da87bcf919c0f37421308bcea2a6d0ff08
ilpovirt <99248645+ilpovirt@users.noreply.github.com>
2024-09-19 10:42:30 +0300

    Add files via upload

---
+++ streamlit_folium_map.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import folium 
+from streamlit_folium import st_folium
+
+df = pd.read_csv("GPSdata.csv")
+
+#Create a map where the center is at start_lat start_long and zoom level is defined
+start_lat = df['Latitude (°)'].mean()
+start_long = df['Longitude (°)'].mean()
+map = folium.Map(location = [start_lat,start_long], zoom_start = 14)
+
+#Draw the map
+folium.PolyLine(df[['Latitude (°)','Longitude (°)']], color = 'blue', weight = 3.5, opacity = 1).add_to(map)
+
+st.title('My journey to work')
+
+#Define map dimensions and show the map
+st_map = st_folium(map, width=900, height=650)
