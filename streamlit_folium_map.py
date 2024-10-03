e1f5ff58a4e5010e4d6dacacaf8381b03e630d4c
ilpovirt <99248645+ilpovirt@users.noreply.github.com>
2024-10-03 11:52:44 +0300

    Update streamlit_folium_map.py

--- streamlit_folium_map.py
+++ streamlit_folium_map.py
@@ -6,8 +6,11 @@ from streamlit_folium import st_folium
 url = "https://raw.githubusercontent.com/ilpovirt/streamlit_map/main/GPSdata.csv"
 df = pd.read_csv(url)
 
-#Arvojen tulostaminen
+st.title('My journey to work')
+
+#Print values
 st.write("Keskinopeus on :", df['Speed (m/s)'].mean(),'m/s' )
+st.write("Kokonaismatka on :", df['Distance (km)'].max(),'km' )
 
 #draw line plot
 st.line_chart(df, x = 'Time (s)', y = 'Distance (km)', y_label = 'Distance',x_label = 'Time')
@@ -20,7 +23,7 @@ map = folium.Map(location = [start_lat,start_long], zoom_start = 14)
 #Draw the map
 folium.PolyLine(df[['Latitude (°)','Longitude (°)']], color = 'blue', weight = 3.5, opacity = 1).add_to(map)
 
-st.title('My journey to work')
+
 
 #Define map dimensions and show the map
 st_map = st_folium(map, width=900, height=650)
